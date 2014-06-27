6497aeab22664e993b6e02a557bfc00b4490189f
mfrasco <mfrasco@uchicago.edu>
2014-06-27 11:11:31 -0500

    I fixed the code, which had difficulies because y was a Factor variable. I created a function from the code. I still need to test its accuracy

--- kNN_iris.R
+++ kNN_iris.R
@@ -20,6 +20,9 @@ n = ncol(iris) - 1
 #Now we will split the iris data into the predictors and the label
 x = iris[,1:n]
 y = iris[,ncol(iris)]
+#I notice that the species column in iris data set is a Factor.
+#This was causing me difficulty, so I convert it to characters.
+y_character = as.character(y)
 
 #We need to normalize all features with mean 0 and variance 1,
 #so that each feature contributes equally to the distance metric.
@@ -29,25 +32,26 @@ for (i in 1:n) {
   x[,i] = (x[,i] - mean(x[,i])) / sd(x[,i])
 }
 
-#Need to create the distance metric, which will be used to measure
-#the distance of a new point from every other point in the dataset.
 #The new point must be four dimensional, with the same features
 #as those in the iris dataset.
-test_point = rep(0,4)
+test_point = rnorm(4)
 
 #I also need to create a distance vector to record the distance
 #from the test point to each point in the data set.
 distances = rep(0,m)
 
-#Here, I create the distance metric
+#Now I will set my value of k.
+k_value = 3
+
+knn_function = function(test_point, k_value){
+
+#Need to create the distance metric, which will be used to measure
+#the distance of a new point from every other point in the dataset.
 for (i in 1:m){
   for (j in 1:n){
     distances[i] = distances[i] + (test_point[j] - x[i,j])^2
   }
 }
-
-#Now I will set my value of k.
-k_value = 3
 
 #In order to know where the k smallest distances are I will use
 #the order function on the distances vector, which returns a vector
@@ -55,16 +59,10 @@ k_value = 3
 #of the smallest element in the original matrix.
 orders = order(distances)
 
-#This doesn't work yet...
-k_lowest_matrix = matrix(rep(0,n*k_value),k_value,n)
-for (i in 1:k_value) {
-  k_lowest_matrix[i,] = x[orders[i],]
-}
-
 #But this works, and this is what matters.
 k_lowest_labels = rep(0,k_value)
 for (i in 1:k_value) {
-  k_lowest_labels[i] = y[orders[i]]
+  k_lowest_labels[i] = y_character[orders[i]]
 }
 
 #Now I will create a scoring algorithm that counts each species in
@@ -72,9 +70,9 @@ for (i in 1:k_value) {
 #to create my score variables
 scores = rep(0,k_value)
 for (i in 1:k_value) {
-  if (k_lowest_labels[i] == 'setosa') {
+  if (k_lowest_labels[i] == "setosa") {
     scores[1] = scores[1] + 1 * distances[orders[i]]
-  } else if (k_lowest_labels[i] == 'versicolor') {
+  } else if (k_lowest_labels[i] == "versicolor") {
     scores[2] = scores[2] + 1 * distances[orders[i]]
   } else scores[3] = scores[3] + 1 * distances[orders[i]]
 }
@@ -82,12 +80,16 @@ for (i in 1:k_value) {
 winner = max(scores)
 if (winner == scores[1]){
   test_point[ncol(iris)] = 'setosa'
+  return(test_point)
   print('The new flower is probably setosa')
 } else if (winner == scores[2]){
   test_point[ncol(iris)] = 'versicolor'
+  return(test_point)
   print('The new flower is probably versicolor')
 } else {
   test_point[ncol(iris)] = 'virginica'
+  return(test_point)
   print('The new flower is probably virginica')
 }
-  
+
+}
